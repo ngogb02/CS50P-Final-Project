83160c68092e6f8f68ecde504ffb2a93ddb96abb
Ngo (US), Bao G <bao.g.ngo@boeing.com>
2024-10-22 15:57:53 -0700

    commit for pyside tutorial_6

--- pyside_tutorial/QPushButton/widget.py
+++ pyside_tutorial/QPushButton/widget.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QMessageBox
 
+
 class Widget(QWidget):
     def __int__(self):
         super().__init__()
@@ -7,4 +8,18 @@ class Widget(QWidget):
         self.setWindowTitle("Custom MainWindow")
 
         button = QPushButton("Click")
-        button.
+        button.clicked.connect(self.button_clicked)
+        button.pressed.connect(self.button_pressed)
+        button.released.connect(self.button_released)
+
+        layout = QVBoxLayout()
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def button_clicked(self):
+        print("Clicked")
+    def button_pressed(self):
+        print("Pressed")
+    def button_released(self):
+        print("Released")
